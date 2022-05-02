0f02b421cde265fa342b2610f9aae70b9c2725e4
Gilbert Pellegrom <gilbert@pellegrom.me>
2022-05-02 15:53:57 +0100

    Update design

--- views/doc.blade.php
+++ views/doc.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="md:flex">
-        <div class="md:w-1/4 mb-12 dark:text-white">
-            <h3 class="text-sm text-gray-500 font-semibold uppercase tracking-wide mb-2">Getting Started</h3>
-            @php
-                $gettingStarted = $staticus
-                    ->content('docs')
-                    ->collection()
-                    ->items->filter(function ($item) {
-                        return $item->getFrontMatter('category') === 'getting-started';
-                    })
-                    ->all();
-            @endphp
-            <ul>
-                @foreach ($gettingStarted as $item)
-                    <li class="doc-{{ basename($item->path) }} mb-1">
-                        <a href="/{{ $item->path }}" class="hover:underline">{{ $item->title }}</a>
-                    </li>
-                @endforeach
-            </ul>
+    <div class="w-full md:flex md:h-screen">
+        <div class="md:w-80 md:h-full pt-16 overflow-auto md:border-r md:border-gray-200 dark:text-white">
+            <div class="px-6 py-12">
+                <h3 class="text-sm text-gray-500 font-semibold uppercase tracking-wide mb-4">Getting Started</h3>
+                @php
+                    $gettingStarted = $staticus
+                        ->content('docs')
+                        ->collection()
+                        ->items->filter(function ($item) {
+                            return $item->getFrontMatter('category') === 'getting-started';
+                        })
+                        ->all();
+                @endphp
+                <ul>
+                    @foreach ($gettingStarted as $item)
+                        <li class="doc-{{ basename($item->path) }} mb-2">
+                            <a href="/{{ $item->path }}"
+                                class="hover:text-blue-600{{ $item->path === $page->path ? ' font-bold' : '' }}">{{ $item->title }}</a>
+                        </li>
+                    @endforeach
+                </ul>
 
-            <h3 class="text-sm text-gray-500 font-semibold uppercase tracking-wide mb-2 mt-10">Creating Content</h3>
-            @php
-                $gettingStarted = $staticus
-                    ->content('docs')
-                    ->collection()
-                    ->items->filter(function ($item) {
-                        return $item->getFrontMatter('category') === 'creating-content';
-                    })
-                    ->all();
-            @endphp
-            <ul>
-                @foreach ($gettingStarted as $item)
-                    <li class="doc-{{ basename($item->path) }} mb-1">
-                        <a href="/{{ $item->path }}" class="hover:underline">{{ $item->title }}</a>
-                    </li>
-                @endforeach
-            </ul>
+                <h3 class="text-sm text-gray-500 font-semibold uppercase tracking-wide mb-4 mt-10">Creating Content</h3>
+                @php
+                    $gettingStarted = $staticus
+                        ->content('docs')
+                        ->collection()
+                        ->items->filter(function ($item) {
+                            return $item->getFrontMatter('category') === 'creating-content';
+                        })
+                        ->all();
+                @endphp
+                <ul>
+                    @foreach ($gettingStarted as $item)
+                        <li class="doc-{{ basename($item->path) }} mb-2">
+                            <a href="/{{ $item->path }}"
+                                class="hover:text-blue-600{{ $item->path === $page->path ? ' font-bold' : '' }}">{{ $item->title }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-        <div class="md:w-3/4">
-            <div class="formatted">
-                <h1>{{ $page->title }}</h1>
-                <div>
-                    {!! $page->html !!}
+        <div class="w-full px-6 md:pt-16 overflow-auto">
+            <div class="max-w-3xl mx-auto py-12">
+                <div class="formatted">
+                    <h1>{{ $page->title }}</h1>
+                    <div>
+                        {!! $page->html !!}
+                    </div>
                 </div>
             </div>
         </div>
--- views/index.blade.php
+++ views/index.blade.php
@@ -3,28 +3,26 @@
 @section('title', 'Staticus - A static site generator in PHP powered by Laravel Blade')
 
 @section('content')
-    <div class="formatted">
-        <p>
-            Staticus is a static site generator in PHP powered by Laravel Blade. Out of the box, Staticus comes with:
-        </p>
-        <ul>
-            <li>Powerful Blade templating</li>
-            <li>Markdown for content-driven pages</li>
-            <li>Laravel Mix to compile your JS &amp; CSS</li>
-        </ul>
-        <p>
-            However, Staticus is designed to be flexible and easy to extend. Almost every feature is optional or can be
-            changed:
-        </p>
-        <ul>
-            <li>Store content in JSON instead of markdown files? No problem.</li>
-            <li>Load content from a remote service like Contentful? Sure.</li>
-            <li>Use a custom CSS framework? Easy peasy.</li>
-        </ul>
-        <p class="not-prose">
-            <a href="/docs/installation" class="inline-block px-6 py-2 mt-6 bg-black text-white no-underline rounded">
-                Get started
-            </a>
-        </p>
+    <div class="page">
+        <div class="formatted">
+            <div class="max-w-lg mx-auto py-12">
+                <h1 class="text-center">Staticus</h1>
+                <p>
+                    Staticus is a static site generator in PHP powered by Laravel Blade. Out of the box, Staticus comes
+                    with:
+                </p>
+                <ul>
+                    <li>Powerful Blade templating</li>
+                    <li>Markdown for content-driven pages</li>
+                    <li>Laravel Mix to compile your JS &amp; CSS</li>
+                </ul>
+                <p class="not-prose text-center">
+                    <a href="/docs/installation"
+                        class="inline-block px-6 py-2 mt-6 bg-black text-white no-underline rounded">
+                        Get started
+                    </a>
+                </p>
+            </div>
+        </div>
     </div>
 @endsection
